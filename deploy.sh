f3334e6b174144c5100ebce04e979bc466d320f8
James Fryer <james@woodheadrestaurantgroup.co.uk>
2025-04-15 17:59:37 +0100

    retrying deploy script to staging. Second attempt

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ THEME_DIR="WH_THEME"
 
 if [ "$1" == "prod" ]; then
   echo "Deploying to PROD site 1"
-  sshpass -p "$SFTP_PASS_PROD_1" rsync -avz --delete \
+  sshpass -p "$CP_SFTP_PASS_PROD" rsync -avz --delete \
     -e "ssh -o StrictHostKeyChecking=no" \
-    $THEME_DIR/ $SFTP_USER_PROD_1@$SFTP_HOST_PROD_1:/wp-content/themes/WH_THEME/
+    $THEME_DIR/ $CP_SFTP_USER_PROD@$CP_SFTP_HOST_PROD:/wp-content/themes/WH_THEME/
 
   # Repeat for other prod sites
 elif [ "$1" == "staging-site-cp" ]; then
   echo "Deploying to CP staging site"
-  sshpass -p "$SFTP_PASS_STAGING_1" rsync -avz --delete \
+  sshpass -p "$CP_SFTP_PASS_STAGING" rsync -avz --delete \
     -e "ssh -o StrictHostKeyChecking=no" \
-    $THEME_DIR/ $SFTP_USER_STAGING_1@$SFTP_HOST_STAGING_1:/wp-content/themes/WH_THEME/
+    $THEME_DIR/ $CP_SFTP_USER_STAGING@$CP_SFTP_HOST_STAGING:/wp-content/themes/WH_THEME/
 fi
