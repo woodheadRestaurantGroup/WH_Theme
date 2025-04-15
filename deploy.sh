e1e1cae1116b16575032ca9f129014b70782257e
James Fryer <james@woodheadrestaurantgroup.co.uk>
2025-04-15 22:08:37 +0100

    staging site deploy test 5

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ if [ "$1" == "prod" ]; then
 elif [ "$1" == "staging-site-cp" ]; then
   echo "Deploying to CP staging site"
 
-  lftp -e "set sftp:connect-program 'ssh -oStrictHostKeyChecking=no'; mirror -R --delete --verbose $THEME_DIR /wp-content/themes/WH_THEME; quit" \
+  lftp -e "set sftp:connect-program 'ssh -oStrictHostKeyChecking=no'; mirror -R --delete --verbose $THEME_DIR /home/RM7qiPYBbqQ8XP/html/wp-content/themes/WH_THEME; quit" \
   -u "$CP_SFTP_USER_STAGING","$CP_SFTP_PASS_STAGING" sftp://$CP_SFTP_HOST_STAGING
 
 fi
