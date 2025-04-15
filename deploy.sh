adc4f304e12e74bcc66e703f2f602ebe343f6cc2
James Fryer <james@woodheadrestaurantgroup.co.uk>
2025-04-15 18:19:03 +0100

    debugging env vars

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,9 @@ if [ "$1" == "prod" ]; then
   # Repeat for other prod sites
 elif [ "$1" == "staging-site-cp" ]; then
   echo "Deploying to CP staging site"
+  echo "SFTP_HOST_STAGING_CP is: ${SFTP_HOST_STAGING_CP:-NOT SET}"
+echo "SFTP_USER_STAGING_CP is: ${SFTP_USER_STAGING_CP:-NOT SET}"
+echo "SFTP_PASS_STAGING_CP is: ${SFTP_PASS_STAGING_CP:+SET}"  # don't echo password
   sshpass -p "$CP_SFTP_PASS_STAGING" rsync -avz --delete \
     -e "ssh -o StrictHostKeyChecking=no" \
     $THEME_DIR/ $CP_SFTP_USER_STAGING@$CP_SFTP_HOST_STAGING:/wp-content/themes/WH_THEME/
